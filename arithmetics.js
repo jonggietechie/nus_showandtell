7c38859d43d59865f56c3b44b2aad1e5dcd4a4fb
Jong Yann <jonggietechie@gmail.com>
2022-12-04 22:49:47 +0800

    To test failed test

--- arithmetics.js
+++ arithmetics.js
@@ -1,6 +1,6 @@
 const add = (a, b) => +a + +b;
 const substract = (a, b) => +a - +b;
-const multiply = (a, b) => a * b;
+const multiply = (a, b) => a / b; //Change to / for negative test
 const divide = (a, b) => (b == '0' ? 'Err' : roundResult(a / b));
 const roundResult = (n) => Math.round((n + Number.EPSILON) * 100) / 100; // Rounds n to 2 decimal places
 
